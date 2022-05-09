c05ce33957cb1cd6c4bf8da96f0d8fb3172c403d
Hoang <quachhoang2002@gmail.com>
2022-05-10 01:27:36 +0700

    update

--- Home/forms.py
+++ Home/forms.py
@@ -2,7 +2,7 @@ from csv import field_size_limit
 import email
 from pyexpat import model
 import django
-from django.forms import ModelForm, PasswordInput
+from django.forms import EmailInput, ModelForm, PasswordInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -11,6 +11,10 @@ from .models import order
 
 
 class CreateUserForm(UserCreationForm):
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg'}))
+    email=forms.EmailField(widget=EmailInput(attrs={'class':'form-control form-control-lg'}))
+    password1=forms.CharField(widget=PasswordInput(attrs={'class':'form-control form-control-lg'}))
+    password2=forms.CharField(widget=PasswordInput(attrs={'class':'form-control form-control-lg'}))
     class Meta:
         model=User
         fields=['username','email','password1','password2']
