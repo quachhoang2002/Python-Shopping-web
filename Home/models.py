f0906dee9a7b1e6904127603712bdda12b908b87
quachhoang2002 <97105329+quachhoang2002@users.noreply.github.com>
2022-05-12 15:56:24 +0700

    update

--- Home/models.py
+++ Home/models.py
@@ -1,5 +1,6 @@
 from distutils.command.upload import upload
 from email.headerregistry import Address
+from email.policy import default
 from pyexpat import model
 import string
 from tkinter import CASCADE
@@ -39,6 +40,7 @@ class order(models.Model):
     phone=models.CharField(max_length=25)
     address=models.CharField(max_length=25)
     create_at=models.TimeField(auto_now=True)
+    status=models.IntegerField(default=0)
     TotalPrice=models.IntegerField()        
     def __str__(self) :
         return str(self.id) 
